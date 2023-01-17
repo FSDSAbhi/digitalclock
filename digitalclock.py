67f6f7e3f83707048a275a9f9f0b7d1dc78bfcd9
Abhilash <Abhilash@workmail.com>
2023-01-17 17:13:45 -0500

    Added background color and welcome message

--- digitalclock.py
+++ digitalclock.py
@@ -4,4 +4,8 @@ window = Tk()
 window.title("Digital Clock")
 window.geometry("600x300")
 
+window.configure(bg="steelblue")
+label = Label(window, text="Welcome!", bg="steelblue")
+label.pack(pady=100)
+
 window.mainloop()
